901001673cd4b5246b24a53530fc44429ae3cb15
Ludovic Courtois <ludovic@lcourtois.fr>
2017-09-27 17:26:39 +0000

    hapi-cron added for future database updating & .env for dev env

--- src/server.js
+++ src/server.js
@@ -20,21 +20,57 @@ server.register(require('inert'), (err) => {
             reply.file('./public/index.html');
         }
     });
-    
-    server.route({
-        method: 'GET',
-        path: '/getmarketcap/{exchange}',
-        handler: ( request, reply ) => {
-            let exchange = encodeURIComponent(request.params.exchange)
-            let ret = null
-            let market = new Market(exchange)
+});
 
-            ret = market.getMarketCap()
-            
-            reply(ret);
+server.register({
+    register: require('hapi-cron'),
+    options: {
+        jobs: [{
+            name: 'testcron',
+            time: '*/60 * * * * *',
+            timezone: 'Europe/London',
+            request: {
+                method: 'GET',
+                url: `/updatedatabase/${process.env.UPDATE_DB_KEY}`
+            },
+            callback: (res) => {
+                console.info('testcron has run!');
+            }
+        }]
+    },
+}, (err) => {
+    if (err) {
+        return console.error(err);
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/getmarketcap/{exchange}',
+    handler: ( request, reply ) => {
+        let exchange = encodeURIComponent(request.params.exchange)
+        let ret = null
+        let market = new Market(exchange)
+
+        ret = market.getMarketCap()
+
+        reply(ret);
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/updatedatabase/{key}',
+    handler: ( request, reply ) => {
+        let key = encodeURIComponent(request.params.key)
+        if (key === process.env.UPDATE_DB_KEY) {
+            console.log('Database updating...')
         }
-    });
-
+        else{
+            console.error(`You can't access here...`)
+        }
+        reply('Database updated')
+    }
 });
 
 server.start(err => {
